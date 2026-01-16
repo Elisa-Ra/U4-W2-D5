e89103bc4ebeea94465897f00f67684836ec7613
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-01-16 11:28:29 +0100

    gestione eccezioni della classe Giochi

--- u4-w2-d5/src/main/java/elisaraeli/entities/Giochi.java
+++ u4-w2-d5/src/main/java/elisaraeli/entities/Giochi.java
@@ -2,14 +2,34 @@ package elisaraeli.entities;
 
 // La mia classe astratta
 public abstract class Giochi {
-    private int id;
+    // L'ID deve essere univoco
+    private static int incremento = 1;
+    private final int id;
     private String title;
     private int annoPubblicazione;
     private double prezzo;
 
+    public Giochi(String title, int annoPubblicazione, double prezzo) {
+        // GESTIONE DELLE ECCEZIONI
+        // controllo che il titolo non sia null o vuoto/formato solo da spazi
+        // uso il throw invece di un sout perché nel sout apparirebbe solo il msg senza "bloccarmi"
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("Errore: titolo non valido.");
+        }
 
-    public Giochi(int id, String title, int annoPubblicazione, double prezzo) {
-        this.id = id;
+        // controllo che l'anno di pubblicazione sia maggiore di 1950 e minore dell'anno corrente
+        int annoCorrente = java.time.LocalDate.now().getYear();
+        if (annoPubblicazione < 1950 || annoPubblicazione > annoCorrente) {
+            throw new IllegalArgumentException("Errore: l'anno di pubblicazione non è valido.");
+        }
+
+        // il prezzo deve avere un valore positivo, quindi controllo che non sia minore/uguale a zero
+        if (prezzo <= 0) {
+            throw new IllegalArgumentException("Errore: il prezzo non è valido.");
+        }
+
+        // per avere un id univoco, ho deciso di mettere l'id che incrementa di volta in volta.
+        this.id = incremento++;
         this.title = title;
         this.annoPubblicazione = annoPubblicazione;
         this.prezzo = prezzo;
@@ -33,7 +53,7 @@ public abstract class Giochi {
 
     @Override
     public String toString() {
-        return "ID: " + id + ", titolo: " + title + "anno di pubblicazione: " + annoPubblicazione
+        return "ID: " + id + ", titolo: " + title + ", anno di pubblicazione: " + annoPubblicazione
                 + ", prezzo: " + prezzo;
     }
 }
