1db19d4c20ad6def97aae4a766cc613c5c25bf9a
Elisa Raeli <79461498+Elisa-Ra@users.noreply.github.com>
2026-01-16 12:17:13 +0100

    adds class GiocoDaTavolo

--- u4-w2-d5/src/main/java/elisaraeli/entities/GiocoDaTavolo.java
+++ u4-w2-d5/src/main/java/elisaraeli/entities/GiocoDaTavolo.java
@@ -3,4 +3,24 @@ package elisaraeli.entities;
 public class GiocoDaTavolo extends Giochi {
     private int numeroGiocatori; // da 2 a 10
     private int durataMedia; // in minuti
+
+    public GiocoDaTavolo(String titolo, int annoPubblicazione, double prezzo,
+                         int numeroGiocatori, int durataMedia) {
+        // Uso i costruttori del padre (Giochi)
+        super(titolo, annoPubblicazione, prezzo);
+
+        // Gestisco le eccezioni
+        if (numeroGiocatori < 2 || numeroGiocatori > 10) {
+            throw new IllegalArgumentException("Errore: Il numero di giocatori deve essere compreso" +
+                    " tra 2 e 10.");
+        }
+
+        if (durataMedia <= 0) {
+            throw new IllegalArgumentException("Errore: la durata deve essere maggiore di 0.");
+        }
+
+        this.numeroGiocatori = numeroGiocatori;
+        this.durataMedia = durataMedia;
+
+    }
 }
